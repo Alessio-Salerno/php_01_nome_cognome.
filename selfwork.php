5b06a60107fa53c4354722454a61baa06c98b831
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-08-01 10:25:01 +0200

    Add files via upload

---
+++ selfwork.php
@@ -0,0 +1,56 @@
+<?php
+
+// INTEGER
+$int = 10;
+
+echo  $int . "\n";
+
+// FLOAT
+
+$float = 45.20;
+
+echo $float . "\n";
+
+
+// STRING
+
+$string = 'NomeCognome';
+
+echo $string . "\n";
+
+
+// BOOLEAN
+
+$boolean = true;
+
+var_dump($boolean);
+
+// COSTANTI
+
+// INT
+
+const INT_CONSTANT = 10;
+
+echo INT_CONSTANT . "\n";
+
+
+// FLOAT 
+
+const FLOAT_CONSTANT = 45.20;
+echo FLOAT_CONSTANT . "\n";
+
+// STRING 
+
+const STRING_CONSTANT = 'NomeCognome';
+echo STRING_CONSTANT . "\n";
+
+
+// BOOLEAN
+
+const BOOLEAN_CONSTANT = true;
+var_dump(BOOLEAN_CONSTANT);
+
+
+
+
+?>
