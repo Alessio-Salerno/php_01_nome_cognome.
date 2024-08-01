b96276d6e772028ee8df5f840a188fc12102cc6e
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-08-01 10:56:44 +0200

    Aggiornato selfwork php

--- selfwork.php
+++ selfwork.php
@@ -1,54 +1,30 @@
 <?php
 
-// INTEGER
+
 $int = 10;
-
-echo  $int . "\n";
-
-// FLOAT
-
 $float = 45.20;
-
-echo $float . "\n";
-
-
-// STRING
-
 $string = 'NomeCognome';
-
-echo $string . "\n";
-
-
-// BOOLEAN
-
 $boolean = true;
 
+var_dump($int);
+var_dump($float);
+var_dump($string);
 var_dump($boolean);
 
-// COSTANTI
 
-// INT
+//  CONSTANTI
 
-const INT_CONSTANT = 10;
-
-echo INT_CONSTANT . "\n";
-
-
-// FLOAT 
-
-const FLOAT_CONSTANT = 45.20;
-echo FLOAT_CONSTANT . "\n";
-
-// STRING 
-
-const STRING_CONSTANT = 'NomeCognome';
-echo STRING_CONSTANT . "\n";
+const INTERO = 10;
+const PUNTO = 45.20;
+const STRINGA = 'NomeCognome';
+const BOOLEANO = true;
 
 
-// BOOLEAN
+var_dump(INTERO);
+var_dump(PUNTO);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
 
-const BOOLEAN_CONSTANT = true;
-var_dump(BOOLEAN_CONSTANT);
 
 
 
